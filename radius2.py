b21e8a75cd2a044869ec5e9df8d856a60624bdb1
kwongliik <46083661+kwongliik@users.noreply.github.com>
2023-05-12 07:02:05 +0800

    comment the codes
    testing purposes

--- radius2.py
+++ radius2.py
@@ -1,19 +1,22 @@
 # function to read input
 def read_input(): 
-    r = float(input("key in radius: "))
-    return r
+    pass
+#     r = float(input("key in radius: "))
+#     return r
 
 # function to calculate the area of circle
 def calculate(r):
-    pi = 3.142
-    area = pi * r ** 2
-    return area
+    pass
+#     pi = 3.142
+#     area = pi * r ** 2
+#     return area
 
 # main
 def main():
-    r = read_input()
-    area = calculate(r)
-    print(f"Area: {area}")
+    pass
+#     r = read_input()
+#     area = calculate(r)
+#     print(f"Area: {area}")
 
 if __name__ == "__main__":
     main() # call main()
